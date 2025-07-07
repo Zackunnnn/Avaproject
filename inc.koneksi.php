136bab92c6ec13f1acd8e7bef51be5ad7a85420f
Zackunnnn <jackone17279@gmail.com>
2025-07-07 20:48:00 +0700

    Bismillah First
    Nyusun file pertama

---
+++ inc.koneksi.php
@@ -0,0 +1,22 @@
+<?php
+
+class Connection{
+	
+   private $host = "localhost";
+   private $struser = "root";
+   private $strpassword = "";
+   private $strdbname = "company2";   
+   public $connection;
+      
+	function __construct() {
+	   $this->connect();
+	}
+	
+	function connect()
+	{
+	    $conn = mysqli_connect($this->host,$this->struser, $this->strpassword);
+		mysqli_select_db($conn, $this->strdbname);
+		$this->connection = $conn;	
+	}
+}
+?> 
